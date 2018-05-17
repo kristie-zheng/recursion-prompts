148032f172a1f2a318a65d616bdc4d3e1c5b11d8
Kristie Desiree <kly5@mail.sfsu.edu>
2018-05-16 22:06:29 -0700

    Implement problems 1-3

--- src/recursion.js
+++ src/recursion.js
@@ -6,17 +6,77 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
+
+//input: a non-negative integer, n
+//output: a non-negative integer representing factorial of n
+//edge cases: none
+//constraints: n must be 0 or positive integer. if negative num, return null
+//base case: when n is 1, return 1
+
+//if n is 1
+  //return 1
+//else
+  //return n times the factorial(n-1)
 var factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  } else if (n >= 1) {
+    return n * factorial(n-1);
+  }
+  else {
+    return null;
+  }
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
+//input: array of integers
+//output: a single integer representing sum of array
+//edge cases: empty array should yield 0
+//single element should return that element
+//constraints: can be positive or negative. must be integer
+//base case: when array's length is zero, return 0
+
+//else if array's length is zero
+  //return zero
+//else 
+  //take the first element of the array
+  //slice the array's first element out
+  //add the first element's value to the result of calling the function recursively on sliced array
+
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  } else {
+    return array[0] + sum(array.slice(1));
+  }
 };
+
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
+//input: an array of numbers and arrays
+//output: a single value representing result of summing all elements
+//edge cases: should return 0 for empty array
+//constraints: no mutation of input
+
+//create a total variable
+//use a for-loop to iterate over the array
+//if element is a number
+  //add to total
+//else if element is another array
+  //call the function recursively
+//return total
 var arraySum = function(array) {
+  var total = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'number') {
+      total += array[i];
+    } else if (Array.isArray(array[i])) {
+      total += arraySum(array[i]);
+    }
+  }
+  return total;
 };
 
 // 4. Check if a number is even.
@@ -119,136 +179,3 @@ var fizzBuzz = function(n) {
 var countOccurrence = function(array, value) {
 };
 
-// 21. Write a recursive version of map.
-// rMap([1,2,3], timesTwo); // [2,4,6]
-var rMap = function(array, callback) {
-};
-
-// 22. Write a function that counts the number of times a key occurs in an object.
-// var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
-// countKeysInObj(obj, 'r') // 1
-// countKeysInObj(obj, 'e') // 2
-var countKeysInObj = function(obj, key) {
-};
-
-// 23. Write a function that counts the number of times a value occurs in an object.
-// var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
-// countValuesInObj(obj, 'r') // 2
-// countValuesInObj(obj, 'e') // 1
-var countValuesInObj = function(obj, value) {
-};
-
-// 24. Find all keys in an object (and nested objects) by a provided name and rename
-// them to a provided new name while preserving the value stored at that key.
-var replaceKeysInObj = function(obj, oldKey, newKey) {
-};
-
-// 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
-// number is the sum of the previous two.
-// Example: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.....
-// fibonacci(5); // [0,1,1,2,3,5]
-// Note: The 0 is not counted.
-var fibonacci = function(n) {
-};
-
-// 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
-// [0,1,1,2,3,5,8,13,21]
-// nthFibo(5); // 5
-// nthFibo(7); // 13
-// nthFibo(3); // 2
-var nthFibo = function(n) {
-};
-
-// 27. Given an array of words, return a new array containing each word capitalized.
-// var words = ['i', 'am', 'learning', 'recursion'];
-// capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
-var capitalizeWords = function(array) {
-};
-
-// 28. Given an array of strings, capitalize the first letter of each index.
-// capitalizeFirst(['car','poop','banana']); // ['Car','Poop','Banana']
-var capitalizeFirst = function(array) {
-};
-
-// 29. Return the sum of all even numbers in an object containing nested objects.
-// var obj1 = {
-//   a: 2,
-//   b: {b: 2, bb: {b: 3, bb: {b: 2}}},
-//   c: {c: {c: 2}, cc: 'ball', ccc: 5},
-//   d: 1,
-//   e: {e: {e: 2}, ee: 'car'}
-// };
-// nestedEvenSum(obj1); // 10
-var nestedEvenSum = function(obj) {
-};
-
-// 30. Flatten an array containing nested arrays.
-// flatten([1,[2],[3,[[4]]],5]); // [1,2,3,4,5]
-var flatten = function(array) {
-};
-
-// 31. Given a string, return an object containing tallies of each letter.
-// letterTally('potato'); // {p:1, o:2, t:2, a:1}
-var letterTally = function(str, obj) {
-};
-
-// 32. Eliminate consecutive duplicates in a list. If the list contains repeated
-// elements they should be replaced with a single copy of the element. The order of the
-// elements should not be changed.
-// compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
-// compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
-var compress = function(list) {
-};
-
-// 33. Augument every element in a list with a new value where each element is an array
-// itself.
-// augmentElements([[],[3],[7]], 5); // [[5],[3,5],[7,5]]
-var augmentElements = function(array, aug) {
-};
-
-// 34. Reduce a series of zeroes to a single 0.
-// minimizeZeroes([2,0,0,0,1,4]) // [2,0,1,4]
-// minimizeZeroes([2,0,0,0,1,0,0,4]) // [2,0,1,0,4]
-var minimizeZeroes = function(array) {
-};
-
-// 35. Alternate the numbers in an array between positive and negative regardless of
-// their original sign. The first number in the index always needs to be positive.
-// alternateSign([2,7,8,3,1,4]) // [2,-7,8,-3,1,-4]
-// alternateSign([-2,-7,8,3,-1,4]) // [2,-7,8,-3,1,-4]
-var alternateSign = function(array) {
-};
-
-// 36. Given a string, return a string with digits converted to their word equivalent.
-// Assume all numbers are single digits (less than 10).
-// numToText("I have 5 dogs and 6 ponies"); // "I have five dogs and six ponies"
-var numToText = function(str) {
-};
-
-
-// *** EXTRA CREDIT ***
-
-// 37. Return the number of times a tag occurs in the DOM.
-var tagCount = function(tag, node) {
-};
-
-// 38. Write a function for binary search.
-// var array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-// binarySearch(array, 5) // 5
-// https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search
-var binarySearch = function(array, target, min, max) {
-};
-
-// 39. Write a merge sort function.
-// mergeSort([34,7,23,32,5,62]) // [5,7,23,32,34,62]
-// https://www.khanacademy.org/computing/computer-science/algorithms/merge-sort/a/divide-and-conquer-algorithms
-var mergeSort = function(array) {
-};
-
-// 40. Deeply clone objects and arrays.
-// var obj1 = {a:1,b:{bb:{bbb:2}},c:3};
-// var obj2 = clone(obj1);
-// console.log(obj2); // {a:1,b:{bb:{bbb:2}},c:3}
-// obj1 === obj2 // false
-var clone = function(input) {
-};

4248108bd99d0d6df16e2ac6b3af40952b66460c
Kristie Desiree <kly5@mail.sfsu.edu>
2018-10-05 00:45:46 -0700

    Implement buildArray

--- src/recursion.js
+++ src/recursion.js
@@ -160,6 +160,13 @@ var reverseArr = function(array) {
 // buildList(0,5) // [0,0,0,0,0]
 // buildList(7,3) // [7,7,7]
 var buildList = function(value, length) {
+  var newArr = [];
+  if (length === 0) {
+    return [];
+  } else {
+    newArr.push(value);
+    return newArr.concat(buildList(value, length - 1));
+  }
 };
 
 // 19. Implement FizzBuzz. Given integer n, return an array of the string representations of 1 to n.
